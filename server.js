ede2ccba0940bb254e5a88d662b32d44fac22763
harsimranjit1 <harsimranjitkaurkhinda@gmail.com>
2024-12-06 23:52:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 /*********************************************************************************
 *  WEB322 – Assignment 05
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source 
-*  (including web sites) or distributed to other students.
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+*  assignment has been copied manually or electronically from any other source (including web sites) or 
+*  distributed to other students.
 * 
-*  Name: ______________________ Student ID: ______________ Date: ________________
-*
+*  Name: HARSIMRANJIT KAUR
+ Student ID: 151966231
+ Date: DEC 6,2024
 *  Cyclic Web App URL: ________________________________________________________
 *
 *  GitHub Repository URL: ______________________________________________________
 *
 ********************************************************************************/ 
+
 
 const express = require("express");
 const path = require("path");
